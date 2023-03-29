fd96e3031a711b8bb551bbac3a7d0e3212077573
aidanmcg67 <115658092+aidanmcg67@users.noreply.github.com>
2023-03-29 12:47:47 -0400

    Add files via upload

--- pwm_driver_test/dump_regs.c
+++ pwm_driver_test/dump_regs.c
@@ -8,30 +8,52 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-struct regs {
+#define IOREGS_OFFSET	0x40008000U
+
+#define USE_DEV_MEM		(1 << 0)
+
+struct regs { 								//TODO: change this
 	volatile uint32_t rate;
 	volatile uint32_t scale;
 	volatile uint32_t samples;
 	volatile uint32_t status_control;
 };
 
-int main(void) {
-	int fd, ret = EXIT_FAILURE;
+int main(int argc, char** argv) {
+	const char* chardev = "/dev/audioctl0";
+	off_t offset = 0;
+	int fd, ret = EXIT_FAILURE, opt;
 	struct regs* ioregs;
-	volatile uint32_t* bram_2;
 
-	if((fd = open("/dev/audioctl0", O_RDWR)) < 0) {
+	while((opt = getopt(argc, argv, "mc:")) != -1) {
+		switch(opt) {
+			case 'm':
+				chardev = "/dev/mem";
+				offset = IOREGS_OFFSET;
+				break;
+			case 'c':
+				chardev = optarg;
+				offset = 0;
+				break;
+			default:
+				fprintf(stderr, "usage: %s [-m] [-c /dev/device_node]\n", argv[0]);
+				return EXIT_FAILURE;
+		}
+	}
+
+	if((fd = open(chardev, O_RDWR)) < 0) {
 		perror("open()");
 		return EXIT_FAILURE;
 	}
 	
-	ioregs = (struct regs*)mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	ioregs = (struct regs*)mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
 
 	if(ioregs == MAP_FAILED) {
 		perror("mmap()");
 		goto err_mmap;
 	}
 	
+	printf("using device %s\n", chardev);
 	printf(
 			"register dump"			"\n"
 			"--------------"		"\n"
